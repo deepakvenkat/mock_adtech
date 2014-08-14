9aaf319df86269cb62da340acca130fe9bc34164
deepakvenkat <dipak2911@gmail.com>
2014-08-14 16:11:54 -0400

    some more things again

---
+++ standalone_survey.js
@@ -0,0 +1,149 @@
+
+
+var initFunctionName = "initContentUnlockWithInternalPlayer";
+var initParams = [ "url", "600", "428", true, "", true, "36", "", "standalone_survey"];
+
+
+
+
+
+var bottom_margin = 125; //Use this variable to move the skip timer up or down. Eg. setting to 30 will move the skip 30px from the bottom
+
+setCunlockSkipButton(true,2,bottom_margin, 5, "http://pix.genesismedia.com/skip.png?CUBS_Standalone_Survey^5544588^23530167^3203993");
+
+cunlockSetBrandBoxMessage("",false);
+
+setCUAdId("5544588", "9876543");
+
+
+var exclusive = false;
+var message = "";
+var skiptime = 2;
+var autoplay = false;
+
+var vast_1 = '<VAST version="2.0">\
+<Ad id="5192728">\
+<Wrapper>\
+\
+\
+\
+<AdSystem>AdTech</AdSystem>\
+<AdTitle>CUBS_Consolidated_VAST_Wrapper</AdTitle>\
+<Impression>\
+<![CDATA[http://ad.crwdcntrl.net/4/to=y|c=1890|p=4648|rand=808180376]]>\
+</Impression>\
+<VASTAdTagURI><![CDATA[http://ad.doubleclick.net/pfadx/N4270.928084.GENESISMEDIA/B8019269.10;sz=0x0;ord=808180376;dcmt=text/xml]]></VASTAdTagURI>\
+\
+\
+<Creatives>\
+<Creative AdID="5192728">\
+<Linear>\
+<Duration>00:00:60</Duration>\
+<TrackingEvents>\
+<Tracking event="start"><![CDATA[http://bcp.crwdcntrl.net/5/c=1890/seg=videostart/seg=campaign^5192728^23262028^abbr=40351,43763,37122,47129,45942,18328,43792,48545]]></Tracking>\
+<Tracking event="start"><![CDATA[http://adserver.adtechus.com/adcount/3.0/5372/3101984/0/16/AdId=5192728;BnId=1;ct=371353746;st=17780;adcid=1;itime=808180376;reqtype=25;]]></Tracking>\
+<Tracking event="start"><![CDATA[http://pix.genesismedia.com/a0bc.png?CUBS_Consolidated_VAST_Wrapper^5192728^23262028^3101984^__SLATEPARAMS__]]></Tracking>\
+<Tracking event="start"><![CDATA[http://b.scorecardresearch.com/p?c1=1&c2=16170130&c3=bs&c5=09&cv=2.0&cj=1]]></Tracking>\
+\
+\
+\
+\
+<Tracking event="firstQuartile"><![CDATA[http://pix.genesismedia.com/t25f.png?CUBS_Consolidated_VAST_Wrapper^5192728^23262028^3101984^__SLATEPARAMS__]]></Tracking>\
+<Tracking event="firstQuartile"><![CDATA[http://bcp.crwdcntrl.net/5/c=1890/b=11645531?CUBS_Consolidated_VAST_Wrapper^5192728^23262028^3101984]]></Tracking>\
+\
+\
+<Tracking event="midpoint"><![CDATA[http://pix.genesismedia.com/f50s.png?CUBS_Consolidated_VAST_Wrapper^5192728^23262028^3101984^__SLATEPARAMS__]]></Tracking>\
+<Tracking event="midpoint"><![CDATA[http://bcp.crwdcntrl.net/5/c=1890/b=11645532?CUBS_Consolidated_VAST_Wrapper^5192728^23262028^3101984]]></Tracking>\
+\
+\
+<Tracking event="thirdQuartile"><![CDATA[http://pix.genesismedia.com/s75t.png?CUBS_Consolidated_VAST_Wrapper^5192728^23262028^3101984^__SLATEPARAMS__]]></Tracking>\
+<Tracking event="thirdQuartile"><![CDATA[http://bcp.crwdcntrl.net/5/c=1890/b=11645533?CUBS_Consolidated_VAST_Wrapper^5192728^23262028^3101984]]></Tracking>\
+\
+\
+<Tracking event="complete"><![CDATA[http://pix.genesismedia.com/o100l.png?CUBS_Consolidated_VAST_Wrapper^5192728^23262028^3101984^__SLATEPARAMS__]]></Tracking>\
+\
+\
+<Tracking event="complete"><![CDATA[http://bcp.crwdcntrl.net/5/c=1890/seg=videoend/seg=campaign^5192728^23262028^abbr=40351,43763,37122,47129,45942,18328,43792,48545]]></Tracking>\
+</TrackingEvents>\
+<VideoClicks>\
+<ClickTracking><![CDATA[http://adserver.adtechus.com/adlink/5372/3101984/0/16/AdId=5192728;BnId=1;itime=808180376;nodecode=yes;link=]]></ClickTracking>\
+<ClickTracking><![CDATA[http://pix.genesismedia.com/click.png?CUBS_Consolidated_VAST_Wrapper^5192728^23262028^3101984^__SLATEPARAMS__]]></ClickTracking>\
+<ClickThrough><![CDATA[http://www.genesismedia.com]]></ClickThrough>\
+</VideoClicks>\
+<MediaFiles>\
+\
+\
+</MediaFiles>\
+</Linear>\
+</Creative>\
+</Creatives>\
+\
+\
+<Extensions>\
+<Extension type="GM">\
+<VisorDuration>3000</VisorDuration>\
+<VisorIconSpacing>8</VisorIconSpacing>\
+<VisorBarHeight>64</VisorBarHeight>\
+<VisorColor>000000</VisorColor>\
+<VisorTracking></VisorTracking>\
+<Branding>\
+<Url></Url>\
+<Logo></Logo>\
+<Message>This is a VAST Wrapper Campaign</Message>\
+<TextColor>#FF00FF</TextColor>\
+<TextSize>14</TextSize>\
+<Font>Verdana,Arial</Font>\
+<Tracking></Tracking>\
+<MultiLine>true</MultiLine>\
+<CustomIcons>\
+<Icon>\
+<Image></Image>\
+<Url></Url>\
+<Tracking></Tracking>\
+</Icon>\
+<Icon>\
+<Image></Image>\
+<Url></Url>\
+<Tracking></Tracking>\
+</Icon>\
+</CustomIcons>\
+</Branding>\
+<AutoPlay>false</AutoPlay>\
+<CountdownDuration>-1</CountdownDuration>\
+<Trackings>\
+<Tracking event="progress" value="15"> <![CDATA[]]></Tracking>\
+<Tracking event="progress" value="15" amount="0.10"> <![CDATA[http://pix.genesismedia.com/billable.png?CUBS_Consolidated_VAST_Wrapper^5192728^23262028^3101984^__SLATEPARAMS__]]></Tracking>\
+</Trackings>\
+</Extension>\
+</Extensions>\
+\
+</Wrapper>\
+\
+\
+\
+</Ad>\
+</VAST>';
+
+var cuEndSlateParams = {
+  enabled : false,
+  logoUrl : "",
+  message : "",
+  main_button : {
+    enabled : false,
+    clickThrough : "",
+    pixel : "",
+    text : "Click here to learn more"
+  },
+  social_buttons : {
+
+
+
+
+
+
+  }
+};
+
+setCUEndSlate(cuEndSlateParams);
+
+
---
+++ vast_with_default_wrapper.js
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<VAST version="2.0">
+<Ad id="4661840">
+<Wrapper>
+<AdSystem>AdTech</AdSystem>
+<VASTAdTagURI>
+  <![CDATA[http://ad3.liverail.com/?LR_PUBLISHER_ID=1331&LR_CAMPAIGN_ID=229&LR_SCHEMA=vast2]]>
+</VASTAdTagURI>
+<Impression><![CDATA[http://adserver.adtechus.com/adcount/3.0/5372/3006182/0/16/AdId=4661840;BnId=1;ct=233184313;st=12548;adcid=1;itime=418446279;reqtype=25;]]></Impression>
+<Creatives>
+<Creative AdID="4661840">
+<Linear>
+<TrackingEvents>
+<Tracking event="start"><![CDATA[http://bcp.crwdcntrl.net/5/c=1890/seg=videostart/seg=campaign^4661840^22819902^abbr=18328^3006182]]></Tracking>
+<Tracking event="start"><![CDATA[http://adserver.adtechus.com/adcount/3.0/5372/3006182/0/16/AdId=4661840;BnId=1;ct=233184313;st=12561;adcid=1;itime=418446279;reqtype=25;]]></Tracking>
+
+<Tracking event="start"><![CDATA[http://pix.genesismedia.com/a0bc.png?CUBS_VASTWrapper_TEST^4661840^22819902^3006182]]></Tracking>
+
+<Tracking event="start"><![CDATA[http://b.scorecardresearch.com/p?c1=1&c2=16170130&c3=bs&c5=09&cv=2.0&cj=1]]></Tracking>
+<Tracking event="firstQuartile"><![CDATA[http://pix.genesismedia.com/t25f.png?CUBS_VASTWrapper_TEST^4661840^22819902^3006182]]></Tracking>
+
+<Tracking event="firstQuartile"><![CDATA[http://bcp.crwdcntrl.net/5/c=1890/b=11645531?CUBS_VASTWrapper_TEST^4661840^22819902^3006182]]></Tracking>
+<Tracking event="midpoint"><![CDATA[http://pix.genesismedia.com/f50s.png?CUBS_VASTWrapper_TEST^4661840^22819902^3006182]]></Tracking>
+
+<Tracking event="midpoint"><![CDATA[http://bcp.crwdcntrl.net/5/c=1890/b=11645532?CUBS_VASTWrapper_TEST^4661840^22819902^3006182]]></Tracking>
+
+<Tracking event="thirdQuartile"><![CDATA[http://pix.genesismedia.com/s75t.png?CUBS_VASTWrapper_TEST^4661840^22819902^3006182]]></Tracking>
+
+<Tracking event="thirdQuartile"><![CDATA[http://bcp.crwdcntrl.net/5/c=1890/b=11645533?CUBS_VASTWrapper_TEST^4661840^22819902^3006182]]></Tracking>
+
+<Tracking event="complete"><![CDATA[http://pix.genesismedia.com/o100l.png?CUBS_VASTWrapper_TEST^4661840^22819902^3006182]]></Tracking>
+
+
+
+
+
+
+
+<Tracking event="complete"><![CDATA[http://bcp.crwdcntrl.net/5/c=1890/seg=videoend/seg=campaign^4661840^22819902^abbr=18328^3006182]]></Tracking>
+</TrackingEvents>
+<VideoClicks>
+<ClickTracking><![CDATA[http://adserver.adtechus.com/adlink/5372/3006182/0/16/AdId=4661840;BnId=1;itime=418446279;nodecode=yes;link=]]></ClickTracking>
+
+<ClickTracking><![CDATA[http://bcp.crwdcntrl.net/5/c=1890/b=11645514^4661840^22819902^abbr=18328^3006182]]></ClickTracking>
+<ClickThrough><![CDATA[www.genesismedia.com]]></ClickThrough>
+</VideoClicks>
+</Linear>
+</Creative>
+</Creatives>
+<Extensions>
+<Extension type="GM">
+<adExchange>liverail</adExchange>
+<Trackings>
+<Tracking event="progress" value="3" amount="0.05"> <![CDATA[http://pix.genesismedia.com/billable.png?CUBS_Consolidated_VAST_Wrapper^5192728^23262028^3101984^__SLATEPARAMS__]]></Tracking>
+</Trackings>
+</Extension>
+</Extensions>
+</Wrapper>
+</Ad>
+</VAST>
